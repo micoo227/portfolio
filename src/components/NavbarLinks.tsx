57a951eccc5ca8e7d2008a24ce2777fd26f57d29
Michael Renda <michaelrenda227@gmail.com>
2025-03-30 15:55:14 -0400

    add text color transition logic to navbar.

--- src/components/NavbarLinks.tsx
+++ src/components/NavbarLinks.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 interface NavbarLinksProps {
 	setIsMenuOpen?: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,25 +8,42 @@ const NavbarLinks = ({ setIsMenuOpen }: NavbarLinksProps) => {
 	const setMenuClosed = () => {
 		if (setIsMenuOpen) setIsMenuOpen(false);
 	};
+	const location = useLocation();
 
 	return (
 		<>
-			<li className="animate-fade-in-0-to-70">
+			<li
+				className={`animate-fade-in-0-to-70 transition hover:text-white ease-out ${
+					location.pathname === "/tech" && "text-white"
+				}`}
+			>
 				<Link to="/tech" onClick={setMenuClosed}>
 					Tech
 				</Link>
 			</li>
-			<li className="animate-fade-in-10-to-80">
+			<li
+				className={`animate-fade-in-10-to-80 transition hover:text-white ease-out ${
+					location.pathname === "/art" && "text-white"
+				}`}
+			>
 				<Link to="/art" onClick={setMenuClosed}>
 					Art
 				</Link>
 			</li>
-			<li className="animate-fade-in-20-to-90">
+			<li
+				className={`animate-fade-in-20-to-90 transition hover:text-white ease-out ${
+					location.pathname === "/info" && "text-white"
+				}`}
+			>
 				<Link to="/info" onClick={setMenuClosed}>
 					Info
 				</Link>
 			</li>
-			<li className="animate-fade-in-30-to-100">
+			<li
+				className={`animate-fade-in-30-to-100 transition hover:text-white ease-out ${
+					location.pathname === "/contact" && "text-white"
+				}`}
+			>
 				<Link to="/contact" onClick={setMenuClosed}>
 					Contact
 				</Link>
